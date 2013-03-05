3213ddea060e5923e093e817a7b76d41a9b6b1df
Channely <qijie29896@gmail.com>
2013-03-05 14:39:22 +0800

    小测试项：实现两数相加

--- spec/plus_two_num_spec.js
+++ spec/plus_two_num_spec.js
@@ -4,4 +4,9 @@
  * Date: 13-3-5
  * Time: 下午2:10
  * To change this template use File | Settings | File Templates.
- */
+**/
+describe("plus two numbers",function(){
+    it("should let two simple numbers plus together",function(){
+        expect(3+6).toEqual(9);
+    });
+});
