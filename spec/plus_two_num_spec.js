910365b1cc5d6659097b3c5f1ba37002b6df73e8
Channely <qijie29896@gmail.com>
2013-03-05 14:56:45 +0800

    t-d-d 模式测试案例1-3

--- spec/plus_two_num_spec.js
+++ spec/plus_two_num_spec.js
@@ -1,10 +1,3 @@
-/**
- * Created with JetBrains RubyMine.
- * User: ccy
- * Date: 13-3-5
- * Time: 下午2:10
- * To change this template use File | Settings | File Templates.
-**/
 describe("plus two numbers",function(){
     it("should let two simple numbers plus together",function(){
         expect(3+6).toEqual(9);
