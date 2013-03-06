9461283b01e7a3dde62be87564bbc753c4ec2c38
Channely <qijie29896@gmail.com>
2013-03-06 13:35:58 +0800

    compare two numbers,case1:num1=num2,get:4A0B

---
+++ spec/compare_two_numbers_spec.js
@@ -0,0 +1,5 @@
+describe("get_similar_rate_of",function(){
+    it("two same numbers,get 4A0B",function(){
+        expect(compare_num(1234,1234)).toBe('4A0B');
+    })
+})
---
+++ src/compare_two_numbers.js
@@ -0,0 +1,5 @@
+function compare_num(num1,num2){
+    if(num1=num2){
+        return '4A0B';
+    }
+}
