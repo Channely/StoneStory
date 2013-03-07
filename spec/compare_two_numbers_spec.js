462e9dca94105d841e5d5bdfd62d4a619465348f
Channely <qijie29896@gmail.com>
2013-03-07 13:33:42 +0800

    两个相近数字比较案例

--- spec/compare_two_numbers_spec.js
+++ spec/compare_two_numbers_spec.js
@@ -5,4 +5,7 @@ describe("Get_similar_rate_of",function(){
     it("two absolutely different numbers 1234 6789,get 0A0B",function(){
         expect(compare_num('1234','6789')).toBe('0A0B');
     });
+    it('two similar numbers 1234 4321',function(){
+        expect(compare_num('1234','4321')).toBe('0A4B');
+    })
 });
