26edaf94dc6540ceec8f0afbd511d1469abc03f4
Channely <qijie29896@gmail.com>
2013-03-07 13:32:47 +0800

    删去旧文件

--- spec/plus_two_num_spec.js
+++
@@ -1,5 +0,0 @@
-describe("plus two numbers",function(){
-    it("should let two simple numbers plus together",function(){
-        expect(3+6).toEqual(9);
-    });
-});
--- src/add_two_num.js
+++
@@ -1,4 +0,0 @@
-function ADD(num1,num2){
-
-    return num1+num2;
-}
