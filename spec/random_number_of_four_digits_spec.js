ac54e0a150d8c8bde5626ba8fce88ffca3a26fb5
Channely <qijie29896@gmail.com>
2013-03-08 00:54:16 +0800

    随机生成一个没有重复数字的四位数 测试+代码

--- spec/random_number_of_four_digits_spec.js
+++ spec/random_number_of_four_digits_spec.js
@@ -1,7 +1,28 @@
-/**
- * Created with JetBrains RubyMine.
- * User: ccy
- * Date: 13-3-7
- * Time: 下午10:35
- * To change this template use File | Settings | File Templates.
- */
+describe('lay out a random number',function(){
+    it('it should be with four digits in it',function(){
+        expect(a_random_num().length).toEqual(4);
+    });
+    it('it should be a random number',function(){
+        var random_num1=a_random_num();
+        var random_num2=a_random_num();
+        expect(random_num1==random_num2).toBeFalsy();
+    });
+    it('it shouldn not have two same numbers within it',function(){
+        var test_1000_times=0;
+        for(k=0;k<1000;k++){
+            var random_num3=a_random_num();
+            var same_counter=0;
+            for(i=0;i<4;i++){
+                for(j=0;j<4;j++){
+                    if(random_num3.charAt(i)==random_num3.charAt(j)){
+                        same_counter++;
+                    }
+                }
+            }
+            if(same_counter==4){
+                test_1000_times++;
+            }
+        }
+        expect(test_1000_times).toEqual(1000);
+    })
+})
--- src/random_number_of_four_digits.js
+++ src/random_number_of_four_digits.js
@@ -1,7 +1,19 @@
-/**
- * Created with JetBrains RubyMine.
- * User: ccy
- * Date: 13-3-7
- * Time: 下午10:36
- * To change this template use File | Settings | File Templates.
- */
+function a_random_num(){
+    for(var h=0;the_same_counter!=4;h++){
+        var the_same_counter=0;
+        var random_num = Math.random();
+        random_num = random_num*1e17;
+        random_num = random_num.toString();
+        random_num = random_num.substr(3,4);
+        for(i=0;i<4;i++){
+            for(j=0;j<4;j++){
+                if(random_num.charAt(i)==random_num.charAt(j)){
+                    the_same_counter++;
+                }
+            }
+        }
+        if(the_same_counter==4){
+            return random_num;
+        }
+    }
+}
