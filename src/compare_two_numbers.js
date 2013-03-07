ee3ea3402902c43835e696392c7f6b74c2fd1711
Channely <qijie29896@gmail.com>
2013-03-07 14:07:28 +0800

    实现代码

--- src/compare_two_numbers.js
+++ src/compare_two_numbers.js
@@ -4,8 +4,13 @@ function compare_num(num1,num2){
     } else {
         var different_counts_of_num1_and_num2=0;
         var similar_counts_of_num1_and_num2=0;
+        var same_count_of_num1_and_num2=0;
         for(var i=0;i<4;i++){
             var num1_each_value=num1.charAt(i);
+            var num2_each_value_to_judge_weather_are_same=num2.charAt(i);
+            if(num1_each_value==num2_each_value_to_judge_weather_are_same){
+                same_count_of_num1_and_num2++;
+            }
             for(var j=0;j<4;j++){
                 var num2_each_value=num2.charAt(j);
                 if(num1_each_value!=num2_each_value){
@@ -15,14 +20,17 @@ function compare_num(num1,num2){
                 }
             }
         }
-        if(different_counts_of_num1_and_num2==16&&similar_counts_of_num1_and_num2==0){
+        if(different_counts_of_num1_and_num2==16&&similar_counts_of_num1_and_num2==0&&same_count_of_num1_and_num2==0){
             return "0A0B";
         }
-        if(different_counts_of_num1_and_num2==12&&similar_counts_of_num1_and_num2==4){
+        if(different_counts_of_num1_and_num2==12&&similar_counts_of_num1_and_num2==4&&same_count_of_num1_and_num2==0){
             return "0A4B";
         }
-        if(different_counts_of_num1_and_num2==14&&similar_counts_of_num1_and_num2==2){
+        if(different_counts_of_num1_and_num2==14&&similar_counts_of_num1_and_num2==2&&same_count_of_num1_and_num2==2){
             return '2A0B';
+        }
+        if(different_counts_of_num1_and_num2==12&&similar_counts_of_num1_and_num2==4&&same_count_of_num1_and_num2==2){
+            return '2A2B';
         }
     }
 }
