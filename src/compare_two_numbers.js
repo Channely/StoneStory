c439eb16d6ca5320f8e70a00fd5f43df89e628f1
Channely <qijie29896@gmail.com>
2013-03-07 13:38:39 +0800

    两个相似数字案例代码实现通过

--- src/compare_two_numbers.js
+++ src/compare_two_numbers.js
@@ -3,17 +3,23 @@ function compare_num(num1,num2){
         return '4A0B';
     } else {
         var different_counts_of_num1_and_num2=0;
+        var similar_counts_of_num1_and_num2=0;
         for(var i=0;i<4;i++){
             var num1_each_value=num1.charAt(i);
             for(var j=0;j<4;j++){
                 var num2_each_value=num2.charAt(j);
                 if(num1_each_value!=num2_each_value){
                     different_counts_of_num1_and_num2++;
+                }else{
+                    similar_counts_of_num1_and_num2++;
                 }
             }
         }
         if(different_counts_of_num1_and_num2==16){
             return "0A0B";
+        }
+        if(different_counts_of_num1_and_num2==12&&similar_counts_of_num1_and_num2==4){
+            return "0A4B";
         }
     }
 }
