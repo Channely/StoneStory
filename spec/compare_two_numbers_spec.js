27239a59b11816e2833dc16294dfe9883a7f233e
Channely <qijie29896@gmail.com>
2013-03-07 13:32:03 +0800

    两个完全不同的数 相比较

--- spec/compare_two_numbers_spec.js
+++ spec/compare_two_numbers_spec.js
@@ -1,5 +1,8 @@
-describe("get_similar_rate_of",function(){
-    it("two same numbers,get 4A0B",function(){
-        expect(compare_num(1234,1234)).toBe('4A0B');
-    })
-})
+describe("Get_similar_rate_of",function(){
+    it("two same numbers 1234 1234,get 4A0B",function(){
+        expect(compare_num('1234','1234')).toBe('4A0B');
+    });
+    it("two absolutely different numbers 1234 6789,get 0A0B",function(){
+        expect(compare_num('1234','6789')).toBe('0A0B');
+    });
+});
--- src/compare_two_numbers.js
+++ src/compare_two_numbers.js
@@ -1,5 +1,19 @@
 function compare_num(num1,num2){
-    if(num1=num2){
+    if (num1 == num2) {
         return '4A0B';
+    } else {
+        var different_counts_of_num1_and_num2=0;
+        for(var i=0;i<4;i++){
+            var num1_each_value=num1.charAt(i);
+            for(var j=0;j<4;j++){
+                var num2_each_value=num2.charAt(j);
+                if(num1_each_value!=num2_each_value){
+                    different_counts_of_num1_and_num2++;
+                }
+            }
+        }
+        if(different_counts_of_num1_and_num2==16){
+            return "0A0B";
+        }
     }
 }
