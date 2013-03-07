fda3542b69dfd8f610c2e3a754a49f16cd3359b4
Channely <qijie29896@gmail.com>
2013-03-07 15:12:20 +0800

    比较功能完成 重新构造

--- spec/compare_two_numbers_spec.js
+++ spec/compare_two_numbers_spec.js
@@ -2,7 +2,7 @@ describe("Get_similar_rate_of",function(){
     it("two same numbers 1234 1234,get 4A0B",function(){
         expect(compare_num('1234','1234')).toBe('4A0B');
     });
-    it("two absolutely different numbers 1234 6789,get 0A0B",function(){
+    it("two different numbers 1234 6789,get 0A0B",function(){
         expect(compare_num('1234','6789')).toBe('0A0B');
     });
     it('two similar numbers 1234 4321,get 0A4B',function(){
--- src/compare_two_numbers.js
+++ src/compare_two_numbers.js
@@ -1,8 +1,4 @@
 function compare_num(num1,num2){
-    if (num1 == num2) {
-        return '4A0B';
-    } else {
-        var different_counts_of_num1_and_num2=0;
         var similar_counts_of_num1_and_num2=0;
         var same_count_of_num1_and_num2=0;
         for(var i=0;i<4;i++){
@@ -13,27 +9,10 @@ function compare_num(num1,num2){
             }
             for(var j=0;j<4;j++){
                 var num2_each_value=num2.charAt(j);
-                if(num1_each_value!=num2_each_value){
-                    different_counts_of_num1_and_num2++;
-                }else{
+                if(num1_each_value==num2_each_value){
                     similar_counts_of_num1_and_num2++;
                 }
             }
         }
-        if(different_counts_of_num1_and_num2==16&&similar_counts_of_num1_and_num2==0&&same_count_of_num1_and_num2==0){
-            return "0A0B";
-        }
-        if(different_counts_of_num1_and_num2==12&&similar_counts_of_num1_and_num2==4&&same_count_of_num1_and_num2==0){
-            return "0A4B";
-        }
-        if(different_counts_of_num1_and_num2==14&&similar_counts_of_num1_and_num2==2&&same_count_of_num1_and_num2==2){
-            return '2A0B';
-        }
-        if(different_counts_of_num1_and_num2==12&&similar_counts_of_num1_and_num2==4&&same_count_of_num1_and_num2==2){
-            return '2A2B';
-        }
-        if(different_counts_of_num1_and_num2==14&&similar_counts_of_num1_and_num2==2&&same_count_of_num1_and_num2==0){
-            return '0A2B';
-        }
-    }
+        return same_count_of_num1_and_num2+'A'+(similar_counts_of_num1_and_num2-same_count_of_num1_and_num2)+'B';
 }
