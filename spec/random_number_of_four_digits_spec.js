11e11e04131edea6f0cda6200ef73302114c68d6
Channely <qijie29896@gmail.com>
2013-03-08 15:19:59 +0800

    改函数名 提取函数

--- spec/random_number_of_four_digits_spec.js
+++ spec/random_number_of_four_digits_spec.js
@@ -1,28 +1,32 @@
 describe('lay out a random number',function(){
     it('it should be with four digits in it',function(){
-        expect(a_random_num().length).toEqual(4);
+        expect(random_a_num().length).toEqual(4);
     });
     it('it should be a random number',function(){
-        var random_num1=a_random_num();
-        var random_num2=a_random_num();
+        var random_num1=random_a_num();
+        var random_num2=random_a_num();
         expect(random_num1==random_num2).toBeFalsy();
     });
     it('it shouldn not have two same numbers within it',function(){
-        var test_1000_times=0;
+        var total_exptected_number_count=0;
         for(k=0;k<1000;k++){
-            var random_num3=a_random_num();
-            var same_counter=0;
-            for(i=0;i<4;i++){
-                for(j=0;j<4;j++){
-                    if(random_num3.charAt(i)==random_num3.charAt(j)){
-                        same_counter++;
-                    }
-                }
-            }
-            if(same_counter==4){
-                test_1000_times++;
+            var random_num=random_a_num();
+            if(is_four_digit_different(random_num)) {
+               total_exptected_number_count++;
             }
         }
-        expect(test_1000_times).toEqual(1000);
-    })
-})
+        expect(total_exptected_number_count).toEqual(1000);
+    });
+
+    function is_four_digit_different(number) {
+        var same_counter=0;
+        for(i=0;i<4;i++){
+            for(j=0;j<4;j++){
+                if(number.charAt(i)==number.charAt(j)){
+                    same_counter++;
+                }
+            }
+        }
+        return same_counter==4;
+    }
+});
--- src/random_number_of_four_digits.js
+++ src/random_number_of_four_digits.js
@@ -1,12 +1,12 @@
-function a_random_num(){
+function random_a_num(){
     for(var h=0;the_same_counter!=4;h++){
         var the_same_counter=0;
         var random_num = Math.random();
         random_num = random_num*1e17;
         random_num = random_num.toString();
         random_num = random_num.substr(3,4);
-        for(i=0;i<4;i++){
-            for(j=0;j<4;j++){
+        for(var i=0;i<4;i++){
+            for(var j=0;j<4;j++){
                 if(random_num.charAt(i)==random_num.charAt(j)){
                     the_same_counter++;
                 }
